1fbddc4c692934c3c14061c9281f67d0f2596c39
Kite <Kite@users.noreply.github.com>
2014-06-22 23:09:45 +0100

    Tidy up.

--- nofacebook.js
+++ nofacebook.js
@@ -1,22 +1,28 @@
-if (document.title.indexOf("Welcome to Facebook") != -1) 
+if( document.title.indexOf( "Welcome to Facebook" ) != -1 ) 
 {
-		var login = document.getElementById( "login_form" );
-		var parentDiv = login.parentNode;
+  var form    = document.getElementById( "login_form" );
+  var formDiv = form.parentNode;
 		
-		parentDiv.onmouseover = function()
+  formDiv.onmouseover = function()
+  {
+    var winWidth = 0;
+		chrome.windows.getLastFocused( function(w) 
 		{
-			
-			
-			var oldX = parentDiv.style.left;
-			var newX = (Math.floor(Math.random() * 1980) + 1);
-			
-			if( newX > 1600 )
-			{
-				newX -= parentDiv.width;
-			}
-			
-			parentDiv.style.position = "absolute";
-			parentDiv.style.top      = "2px";
-			parentDiv.style.left     = newX + "px";
-		};
+			winWidth = window.innerWidth;
+	  } );
+		
+    var oldX = formDiv.style.left;
+    var newX = (Math.floor(Math.random() * winWidth) + 1);
+		
+		
+		
+    if( newX > winWidth - formDiv.width )
+    {
+      newX -= formDiv.width;
+    }
+
+    formDiv.style.position = "absolute";
+    formDiv.style.top      = "2px";
+    formDiv.style.left     = newX + "px";
+  };
 }
